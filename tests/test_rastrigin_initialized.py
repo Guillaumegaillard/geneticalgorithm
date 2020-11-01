f02bfedff2f3833eaf96f6003b08bca6785a9835
Guillaumegaillard <guillaume.gaillard.maze@gmail.com>
2020-11-01 10:31:48 +0100

    tested child unzipping

--- tests/test_rastrigin_initialized.py
+++ tests/test_rastrigin_initialized.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('..')
+sys.path=['..']+sys.path
 
 import numpy as np
 import math
@@ -8,31 +8,31 @@ from geneticalgorithm import geneticalgorithm as ga
 
 def f(X):
     dim = len(X)
-    OF = 0
-    for i in range (0, dim):
-        OF+=(X[i]**2)-10*math.cos(2*math.pi*X[i])+10
+    OF = np.sum((X**2)-10*np.cos(2*math.pi*X)+10)
     return OF
 
 
-def test_rastrigin():
-    parameters={'max_num_iteration': 1000,
-                'population_size':200,
+def test_rastrigin_initialized():
+    parameters={'max_num_iteration': 100,
+                'population_size':2000,
                 'mutation_probability':0.1,
                 'elit_ratio': 0.02,
                 'crossover_probability': 0.5,
                 'parents_portion': 0.3,
                 'crossover_type':'two_point',
                 'max_iteration_without_improv':None,
-                'multiprocessing_ncpus': 8,
+                'multiprocessing_ncpus': 4,
                 'multiprocessing_engine': None,
                 }
-    varbound = np.array([[-5.12, 5.12]]*2)
 
-    model = ga(function=f, dimension=2, variable_type='real',
+    dimension=200
+    varbound = np.array([[-5.12, 5.12]]*dimension)
+
+    model = ga(function=f, dimension=dimension, variable_type='real',
             variable_boundaries=varbound, algorithm_parameters=parameters)
-    model.run(initial_idv=np.array([-7.27357062e-07,1.54894047e-09]))
-    assert model.best_function < 1e-6
+    model.run(plot=True,initial_idv=np.random.random(size=dimension)*5.12-10.24)
+    assert model.best_function < 1000
 
 
 if __name__ == '__main__':
-    test_rastrigin()
+    test_rastrigin_initialized()
